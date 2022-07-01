3acb4a7c32c14981fda96958f33fcf9bcbfdc4a5
raj_kayasth <rajakayasth19@gmail.com>
2022-07-01 11:24:18 +0530

    iterceptros

--- app/src/main/java/com/example/retrofitdemo/apiService/NewsService.kt
+++ app/src/main/java/com/example/retrofitdemo/apiService/NewsService.kt
@@ -1,12 +1,13 @@
 package com.example.retrofitdemo.apiService
 
 import android.os.Build
-
 import okhttp3.CacheControl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
+
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -26,19 +27,49 @@ class RetrofitsClient {
         private var instance: Api? = null
         private var retroFitInstance: Retrofit? = null
 
-        private fun retroInstance(): Retrofit {
+
+      private  fun retroInstance(): Retrofit {
+
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val cacheInterceptor = object : Interceptor {
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val response: Response = chain.proceed(chain.request())
+                    val cacheControl = CacheControl.Builder()
+                        .maxAge(1, TimeUnit.DAYS)
+                        .build()
+                    return response.newBuilder()
+                        .header("Cache-Control", cacheControl.toString())
+                        .build()
+                }
+
+            }
+            val headerInterceptor = Interceptor { chain ->
+                var request = chain.request()
+                request = request.newBuilder()
+                    .addHeader("x-device-type", Build.DEVICE)
+                    .build()
+
+                chain.proceed(request)
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addNetworkInterceptor(cacheInterceptor)
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(loggingInterceptor)
+                .build()
 
 
-            if (retroFitInstance == null){
+            if (retroFitInstance == null) {
                 return Retrofit.Builder()
-                    .baseUrl(BASE_URL2)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL2)
+                    .client(okHttpClient)
                     .build()
             }
             return retroFitInstance as Retrofit
         }
-
-
 
         @Synchronized
         fun getInstance(): Api {
